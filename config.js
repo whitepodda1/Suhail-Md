317deef57d1e754ca4fe8c083d4759c9317cd828
whitepodda1 <166819436+whitepodda1@users.noreply.github.com>
2024-04-18 22:25:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
 
   aitts_Voice_Id : process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY  || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "publick",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "public",
   LANG: ( process.env.THEME ||  "SUHAIL"  ).toUpperCase(),
 
 
